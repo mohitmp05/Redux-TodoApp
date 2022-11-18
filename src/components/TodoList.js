f48757e1edfea22d42faf5ce7ab76a9190066bcf
Mohit <officialmohit05@gmail.com>
2022-11-18 12:30:20 +0530

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+	const todos = useSelector((state)=> state.todos);
+
+	return (
+		<ul className='list-group'>
+			{todos.map((todo) => (
+				<TodoItem key={todo.id} id={todo.id} title={todo.title} completed={todo.completed} />
+			))}
+		</ul>
+	);
+};
+
+export default TodoList;
